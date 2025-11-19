83baa338fb9260518f7bfc3816dbfa278bd7113d
Muzamilafey <muzamilinc@gmail.com>
2025-11-19 20:17:47 +0300

    167

--- src/App.js
+++ src/App.js
@@ -110,7 +110,7 @@ function App() {
           <Route path="/" element={<Navigate to="/login" />} />
 
           <Route path="/login" element={<Login />} />
-          <Route path="/register" element={<Register />} />
+          
           
           <Route path="/verify-email" element={<VerifyEmail />} />
           <Route path="/forgot-password" element={<ForgotPassword />} />
